2e4952cca5caa4b438c3a36b2769aa6899a49cb8
Josh Grimaldi <69489227+mabejohnny@users.noreply.github.com>
2020-10-05 16:14:25 -0400

    Added my member variables and constructor for my Node Class.  Set up my member variables, constructor for my BinarySearchTree Class as well as started working on the functionality of my Add()

---
+++ binarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binarySearchTree
+{
+    class BinarySearchTree
+    {
+        //Member Variables
+
+        public Node root;
+
+        //Constructor
+        public BinarySearchTree()
+        {
+            root = null; 
+        }
+
+        //Member Methods
+
+        public void Add(int valueToInsert)
+        {
+            Node nodeToAdd = new Node(valueToInsert);
+
+            if(root == null)
+            {
+                root = nodeToAdd; 
+            }
+            else
+            { 
+                Node currentNode = root;
+
+                if(currentNode.data < valueToInsert)
+                {
+                    while (true)
+                    {
+                        currentNode = currentNode.left;
+                        currentNode.left = nodeToAdd;
+                    }
+                }
+                else if(currentNode.data > valueToInsert)
+                {
+                    while (true)
+                    {
+                        currentNode = currentNode.right;
+                        currentNode.right = nodeToAdd;
+                    }
+                }
+              
+            }
+        }
+    }
+}
