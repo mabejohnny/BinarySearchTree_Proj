5fb0ce3f1b56e0330e7a70f66eaad6bcfb3effbd
Josh Grimaldi <69489227+mabejohnny@users.noreply.github.com>
2020-10-06 09:56:01 -0400

    Added my search function for binary search tree.  Next course of action will be to create nodes and test both methods.

--- binarySearchTree/BinarySearchTree.cs
+++ binarySearchTree/BinarySearchTree.cs
@@ -59,10 +59,63 @@ namespace binarySearchTree
                             currentNode = currentNode.right;
                         }
                     }
-
                 }
-              
             }
+        }
+
+        //first check if value equals root, if so return true
+        //else have a while loop that is !saveItem and goes through tree to find value
+        // add a way for the loop to end when the value is found or if left or right == null;
+        public bool Search(int valueToCheck)
+        {
+            bool saveItem = false;
+
+            if (valueToCheck == root.data)
+            {
+                return true; //value found was the root 
+            }
+            else
+            {
+                while (!saveItem)
+                {
+                    Node currentNode = root; 
+
+                    if(valueToCheck <= currentNode.data)
+                    {
+                        if(currentNode.left == null)
+                        {
+                            return false; //value was not found at left 
+                        }
+                        else if(valueToCheck == currentNode.left.data)
+                        {
+                            saveItem = true;//stops while loop found value
+                            return true;
+                        }
+                        else
+                        {
+                            currentNode = currentNode.left;
+                        }
+                    }
+                    else   //checking right 
+                    {
+                        if(currentNode.right == null)
+                        {
+                            return false;
+                        }
+                        else if(valueToCheck == currentNode.right.data)
+                        {
+                            saveItem = true;
+                            return true;
+                        }
+                        else
+                        {
+                            currentNode = currentNode.right; 
+                        }
+                    }
+                }
+            }
+            return false;
+
         }
     }
 }
