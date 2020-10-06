b23a4b4b0e3eca0895069fa3f03eb5b74fcd53b3
Josh Grimaldi <69489227+mabejohnny@users.noreply.github.com>
2020-10-06 11:27:50 -0400

    I have been commiting but my search tree class does not seem to populate of GitHub

--- binarySearchTree/Program.cs
+++ binarySearchTree/Program.cs
@@ -10,6 +10,14 @@ namespace binarySearchTree
     {
         static void Main(string[] args)
         {
+            BinarySearchTree addToTree = new BinarySearchTree();
+            addToTree.Add(100);
+            addToTree.Add(50);
+            addToTree.Add(120);
+            addToTree.Add(25);
+
+            addToTree.Search(25);
+
         }
     }
 }
